a9c1608e94f4ac1beeb8d0b216dd3c52159ac508
Kamil Patena <kamilpatena1221@gmail.com>
2016-02-10 10:25:43 -0800

    part of step4

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -27,10 +27,16 @@ class Order extends Application {
         if ($order_num == null)
             redirect('/order/neworder');
 
+        $order = $this->orders->get($order_num).
+
         $this->data['pagebody'] = 'show_menu';
         $this->data['order_num'] = $order_num;
         //FIXME
-
+        $this->data['title'] = "Order # " . $order_num
+            .
+            ' (' .
+            number_format($this->orders->total($order_num),
+                2) . ')';
         // Make the columns
         $this->data['meals'] = $this->make_column('m');
         $this->data['drinks'] = $this->make_column('d');
@@ -64,6 +70,7 @@ class Order extends Application {
     // make a menu ordering column
     function make_column($category) {
         //FIXME
+        $items = $this->menu->some('category',$category);
         return $items;
     }
 
